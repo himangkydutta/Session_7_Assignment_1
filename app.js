5a8b24efdd78a786102f5ae4ff037e350d558d3a
himangkydutta <himangkydutta08@gmail.com>
2017-05-04 18:47:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+//Here function calculateArea() which takes radius as parameter and returns area of circle
+
+function calculateArea(radius)
+{
+    return(radius*radius*Math.PI);
+}
+
+var circle=calculateArea(7);
+console.log(calculateArea(7));
+console.log('Area of circle is :- '+circle);
+	
